ada06d077669c0cd2dfcfb16f4f54a39865a0cb5
mrmxs <maryzmaxs@gmail.com>
2022-02-08 21:00:24 +0300

    edit HealthFacilityBranch: Properties HealthFacilityId, Address, Type Added. Edit Entity LabMedicalTest: Property LabId Renamed. Migration

---
+++ src/Infrastructure/Migrations/20220208174948_EntityHealthFacilityBranchPropertiesAddedAndLabMedicalTestPropertyEdited.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthPanel.Infrastructure.Migrations
+{
+    public partial class EntityHealthFacilityBranchPropertiesAddedAndLabMedicalTestPropertyEdited : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "lab_id",
+                table: "lab_tests",
+                newName: "health_facility_branch_id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "address",
+                table: "health_facility_branches",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "health_facility_id",
+                table: "health_facility_branches",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                table: "health_facility_branches",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "address",
+                table: "health_facility_branches");
+
+            migrationBuilder.DropColumn(
+                name: "health_facility_id",
+                table: "health_facility_branches");
+
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "health_facility_branches");
+
+            migrationBuilder.RenameColumn(
+                name: "health_facility_branch_id",
+                table: "lab_tests",
+                newName: "lab_id");
+        }
+    }
+}
