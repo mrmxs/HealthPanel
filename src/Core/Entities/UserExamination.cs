e4e70fdbaba2c601b904182c1fcb54651e10c3fd
mrmxs <maryzmaxs@gmail.com>
2022-02-10 00:24:11 +0300

    add UserExamination entity

---
+++ src/Core/Entities/UserExamination.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using HealthPanel.Core.Enums;
+
+namespace HealthPanel.Core.Entities
+{
+    public class UserExamination
+    {
+        public int Id { get; internal set; }        
+        [Required]
+        public int ExaminationId { get; set; } 
+        [Required] 
+        public int UserId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime Date { get; set; }
+        public string Value { get; set; }
+        public TestStatus Status { get; set; }
+        
+    }
+}
