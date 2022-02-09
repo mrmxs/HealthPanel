e95ce5428c91ed83af6d3d107f1377b57c421311
mrmxs <maryzmaxs@gmail.com>
2022-02-10 00:25:57 +0300

    add migration EntityUserExaminationCreated

--- src/Infrastructure/Data/HealthPanelDbContext.cs
+++ src/Infrastructure/Data/HealthPanelDbContext.cs
@@ -13,6 +13,7 @@ namespace HealthPanel.Infrastructure.Data
         public DbSet<LabMedTest> LabTests { get; set; }
         public DbSet<Examination> Examinations { get; set; }
         public DbSet<UserLabTest> UserLabTests { get; set; }
+        public DbSet<UserExamination> UserExaminations { get; set; }
 
         public DbSet<HealthFacility> HealthFacilities { get; set; }
         public DbSet<HealthFacilityBranch> HealthFacilityBranches { get; set; }
---
+++ src/Infrastructure/Migrations/20220209212501_EntityUserExaminationCreated.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HealthPanel.Infrastructure.Migrations
+{
+    public partial class EntityUserExaminationCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user_examinations",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    examination_id = table.Column<int>(type: "integer", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    doctor_id = table.Column<int>(type: "integer", nullable: false),
+                    date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    value = table.Column<string>(type: "text", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_examinations", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_examinations");
+        }
+    }
+}
