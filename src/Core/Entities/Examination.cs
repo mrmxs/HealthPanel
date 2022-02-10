d03a632f11f439b721fe7046b195b7a211ec81ca
mrmxs <maryzmaxs@gmail.com>
2022-02-10 18:47:18 +0300

    add property CustomName to LabMedTest & Examination models. Migration

--- src/Core/Entities/Examination.cs
+++ src/Core/Entities/Examination.cs
@@ -5,5 +5,6 @@ namespace HealthPanel.Core.Entities
         public int Id { get; internal set; }
         public int HealthFacilityBranchId { get; set; }  // todo (с типом Hospital/Clinic ?)
         public int TestId { get; set; }
+        public string CustomName { get; set; }
     }
 }
--- src/Infrastructure/Migrations/20220210051713_EntitiesLabMadTestAndExaminationPropertyCustomNameAdded.Designer.cs
+++ src/Infrastructure/Migrations/20220210051713_EntitiesLabMadTestAndExaminationPropertyCustomNameAdded.Designer.cs
@@ -2,6 +2,7 @@
 using HealthPanel.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HealthPanel.Infrastructure.Migrations
 {
     [DbContext(typeof(HealthPanelDbContext))]
-    partial class HealthPanelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220210051713_EntitiesLabMadTestAndExaminationPropertyCustomNameAdded")]
+    partial class EntitiesLabMadTestAndExaminationPropertyCustomNameAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
