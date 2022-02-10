6ad4d2f43476a7d4809385d791efb217d5ed6c17
mrmxs <maryzmaxs@gmail.com>
2022-02-10 18:50:58 +0300

    add property CustomName to LabMedTestDto & ExaminationDto. Edit COntrollers. Fix REST

--- src/Services/Stats/Dtos/ExaminationDto.cs
+++ src/Services/Stats/Dtos/ExaminationDto.cs
@@ -4,6 +4,9 @@ namespace HealthPanel.Services.Stats.Dtos
     {
         public int Id { get; internal set; }
         public int HealthFacilityBranchId { get; set; }  // todo (с типом Hospital/Clinic ?)
+        public string HealthFacilityBranchName { get; set; }
         public int TestId { get; set; }
+        public string TestName { get; set; }
+        public string CustomTestName { get; set; }
     }
 }
