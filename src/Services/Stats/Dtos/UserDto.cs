27ae55c698a5c4351ef7d548699e561190a255a7
mrmxs <maryzmaxs@gmail.com>
2022-02-12 01:17:43 +0300

    add UserDto ctor from entities. Edit Controller's EntityToDto method

--- src/Services/Stats/Dtos/UserDto.cs
+++ src/Services/Stats/Dtos/UserDto.cs
@@ -1,8 +1,16 @@
+using HealthPanel.Core.Entities;
+
 namespace HealthPanel.Services.Stats.Dtos
 {
-    public class UserDto: IDto
+    public class UserDto : IDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }        
+        public string Name { get; set; } 
+
+        public UserDto(User userEntity)
+        {
+            this.Id = userEntity.Id;
+            this.Name = userEntity.Name;        
+        }
     }
 }
