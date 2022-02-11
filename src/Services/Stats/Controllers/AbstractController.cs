b4e2bb6082c7b7bf944b3af49cfcd876975d3ea5
mrmxs <maryzmaxs@gmail.com>
2022-02-12 00:18:24 +0300

    edit AbstractController. Add EntityToDto method

--- src/Services/Stats/Controllers/AbstractController.cs
+++ src/Services/Stats/Controllers/AbstractController.cs
@@ -13,27 +13,32 @@ namespace HealthPanel.Services.Stats.Controllers //Stats.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public abstract class AbstractController<T> : ControllerBase where T : IDto
+    public abstract class AbstractController<T,D> : ControllerBase 
+            where T : IEntity
+            where D : IDto
     {
         // GET api/base
         [HttpGet]
-        public abstract Task<ActionResult<IEnumerable<T>>> Get();
+        public abstract Task<ActionResult<IEnumerable<D>>> Get();
 
         // GET api/base/5
         [HttpGet("{id}")]
-        public abstract Task<ActionResult<T>> Get(int id);
+        public abstract Task<ActionResult<D>> Get(int id);
 
         // POST api/base
         [HttpPost]
-        public abstract Task<ActionResult<T>> Post([FromBody] T value);
+        public abstract Task<ActionResult<D>> Post([FromBody] D value);
 
         // PUT api/base/5
         [HttpPut("{id}")]
-        public abstract Task<IActionResult/*ActionResult<T>*/> Put(int id, [FromBody] T value);
+        public abstract Task<IActionResult> Put(int id, [FromBody] D value);
 
         // DELETE api/base/5
         [HttpDelete("{id}")]
-        public abstract Task<IActionResult/*ActionResult<T>*/> Delete(int id);
+        public abstract Task<IActionResult> Delete(int id);
+
+
+        protected abstract Task<D> EntityToDto(T entity);
 
         protected BadRequestObjectResult CustomBadRequest(object error)
         {
