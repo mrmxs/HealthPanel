9dcc1867bb017e46b29e6303dc84bf73f38abd98
mrmxs <maryzmaxs@gmail.com>
2022-02-12 02:02:58 +0300

    add ExaminationDto ctor from entities. Edit Controller's EntityToDto method

--- src/Services/Stats/Dtos/ExaminationDto.cs
+++ src/Services/Stats/Dtos/ExaminationDto.cs
@@ -1,3 +1,5 @@
+using HealthPanel.Core.Entities;
+
 namespace HealthPanel.Services.Stats.Dtos
 {
     public class ExaminationDto : IDto
@@ -8,5 +10,18 @@ namespace HealthPanel.Services.Stats.Dtos
         public int TestId { get; set; }
         public string TestName { get; set; }
         public string CustomTestName { get; set; }
+
+        public ExaminationDto(
+            Examination examinationEntity,
+            HealthFacilityBranch branchEntity,
+            MedTest medTestEntity)
+        {
+            this.Id = examinationEntity.Id;
+            this.HealthFacilityBranchId = examinationEntity.HealthFacilityBranchId;
+            this.HealthFacilityBranchName = branchEntity.Name;
+            this.TestId = examinationEntity.TestId;
+            this.TestName = medTestEntity.Name;
+            this.CustomTestName = examinationEntity.CustomName;
+        }
     }
 }
