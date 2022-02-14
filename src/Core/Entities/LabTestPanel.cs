686610604f6a01a94599b835345608f43d3bdfcc
mrmxs <maryzmaxs@gmail.com>
2022-02-14 15:41:04 +0300

    add TestPanel and LabTestPanel entities

---
+++ src/Core/Entities/LabTestPanel.cs
@@ -0,0 +1,15 @@
+namespace HealthPanel.Core.Entities
+{
+    /// <summary>
+    /// Группа/панель тестов, выпоняемая организацией одномометно
+    /// </summary>
+    public class LabTestPanel : IEntity
+    {
+        public int Id { get; internal set; }
+        public int HealthFacilityBranchId { get; set; }
+        public string? CustomName { get; set; }
+        public int?[]? LabTestIds { get; set; }
+    }
+
+
+}
