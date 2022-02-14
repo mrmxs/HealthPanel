e1d4826bc8126b65a8c97c5b2c5451d146207778
mrmxs <maryzmaxs@gmail.com>
2022-02-14 15:41:34 +0300

    add migration  EntitiesTestPanelAndLabTestPanelCreated

---
+++ src/Infrastructure/Migrations/20220214123145_EntitiesTestPanelAndLabTestPanelCreated.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HealthPanel.Infrastructure.Migrations
+{
+    public partial class EntitiesTestPanelAndLabTestPanelCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "lab_test_panels",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    health_facility_branch_id = table.Column<int>(type: "integer", nullable: false),
+                    custom_name = table.Column<string>(type: "text", nullable: true),
+                    lab_test_ids = table.Column<int?[]>(type: "integer[]", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_lab_test_panels", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "test_panels",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_test_panels", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "lab_test_panels");
+
+            migrationBuilder.DropTable(
+                name: "test_panels");
+
+        }
+    }
+}
