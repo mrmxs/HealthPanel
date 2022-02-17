7c6cd0f2a943c87870b485c91eb4eab7a8cff26b
mrmxs <maryzmaxs@gmail.com>
2022-02-17 17:35:36 +0300

    add TestToTestListDto, TestToTestListController

---
+++ src/Services/Stats/Dtos/TestToTestListDto.cs
@@ -0,0 +1,30 @@
+using HealthPanel.Core.Entities;
+
+namespace HealthPanel.Services.Stats.Dtos
+{
+    public class TestToTestListDto : IDto
+    {
+        public int Id { get; set; }
+        public int TestListId { get; set; }
+        public int MedTestId { get; set; }
+        public int LabTestId { get; set; }
+        public int ExaminationId { get; set; }
+        public int TestPanelId { get; set; }
+        public int LabTestPanelId { get; set; }
+        public int Index { get; set; }
+
+        public TestToTestListDto() { }
+
+        public TestToTestListDto(TestToTestList entity)
+        {
+            this.Id = entity.Id;
+            this.TestListId = entity.TestListId;
+            this.MedTestId = entity.MedTestId;
+            this.LabTestId = entity.LabTestId;
+            this.ExaminationId = entity.ExaminationId;
+            this.TestPanelId = entity.TestPanelId;
+            this.LabTestPanelId = entity.LabTestPanelId;
+            this.Index = entity.Index;
+        }
+    }
+}
