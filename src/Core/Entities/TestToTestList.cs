1c612ff9373294051d857f2835efcea31c0674e8
mrmxs <maryzmaxs@gmail.com>
2022-02-17 15:55:52 +0300

    add TestLIst and TestToTestList entities

---
+++ src/Core/Entities/TestToTestList.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthPanel.Core.Entities
+{
+    /// <summary>
+    /// Сущность для привязки теста/исследования/панели к списку тестов
+    /// </summary>
+    public class TestToTestList : IEntity
+    {
+        public int Id { get; internal set; }
+        [Required]
+        public int TestListId { get; internal set; }
+        public int MedTestId { get; set; } // EX: список для госпитализации
+        public int LabTestId { get; set; } 
+        public int ExaminationId { get; set; }
+        public int TestPanelId { get; set; }
+        public int LabTestPanelId { get; set; }
+        public int Index { get; set; }
+    }
+}
